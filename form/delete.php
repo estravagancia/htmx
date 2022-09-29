43b886cef8012b22a34951a85ac6b39dc723e836
estravagancia <webmaster@estravagancia.com>
2022-09-29 17:34:38 +0100

    hx-delete con hx-vals metodo DELETE

---
+++ form/delete.php
@@ -0,0 +1,10 @@
+<?php 
+$data = file_get_contents("php://input");
+
+foreach (explode('&', $data) as $chunk) {
+    $param = explode("=", $chunk);
+    if ($param) {
+        printf("Value for parameter \"%s\" is \"%s\"<br/>\n", urldecode($param[0]), urldecode($param[1]));
+    }
+}
+?>
