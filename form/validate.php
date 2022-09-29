39d6146d3dc3187742fc8037d1172ddab1e85ed3
estravagancia <webmaster@estravagancia.com>
2022-09-29 13:39:01 +0100

    Update validate.php
    comentada una de las dos opciones de comprobar el nombre

--- form/validate.php
+++ form/validate.php
@@ -4,8 +4,8 @@ if (isset($_REQUEST))
 {	
 	foreach ($_REQUEST as $key => $value) {
 		if($key ==='name'){
-			if (!preg_match("/^[a-zA-Z-' ]*$/",$value)) {
-				echo "Sólo letras (sin acentos) y espacios en blanco permitidos";
+			//if (!preg_match("/^[a-zA-Z-' ]*$/",$value)) {
+			//	echo "Sólo letras (sin acentos) y espacios en blanco permitidos";
 			if (!preg_match("/^(?=.{3,18}$)[a-zñA-ZÑ](\s?[a-zñA-ZÑ])*$/",$value)) {
 				echo "Sólo letras y espacios en blanco permitidos entre 3 y 18 caracteres";
 				continue;
