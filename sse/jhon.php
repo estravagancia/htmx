814e5da419e0d873f95f7c20e303c5a3ee50eebe
estravagancia <webmaster@estravagancia.com>
2022-09-30 18:37:21 +0100

    SSE con nombre de evento

--- sse/jhon.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
-
-$details = array();
-$details['name'] = "john doe";
-echo "data: " . json_encode($details) . "\n\n";
-flush();
-?>
