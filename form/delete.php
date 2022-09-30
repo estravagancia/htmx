04e861a1dd176a513206e754b149bb6fb0d09a9d
estravagancia <webmaster@estravagancia.com>
2022-09-30 18:02:33 +0100

    cambios usando include en lugar de repitiendo

--- form/delete.php
+++ form/delete.php
@@ -7,42 +7,5 @@
 //         printf("Value for parameter \"%s\" is \"%s\"<br/>\n", urldecode($param[0]), urldecode($param[1]));
 //     }
 // }
-
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-function parseInput()
-{
-    $data = file_get_contents("php://input");
-    if($data == false)
-        return array();
-    parse_str($data, $result);
-    return $result;
-}
-
-switch ($method)
-{
-    case 'GET':
-        echo "GET request method\n";
-        echo print_r($_GET, true);
-        break;
-    case 'POST':
-        echo "POST request method\n";
-        echo print_r($_POST, true);
-        break;
-    case 'PUT':
-        $_PUT = parseInput();
-
-        echo "PUT request method\n";
-        echo print_r($_PUT, true);
-        break;
-    case 'DELETE':
-        $_DELETE = parseInput();
-        echo "DELETE request method\n";
-        echo print_r($_DELETE, true);
-        break;
-    default:
-        echo "Unknown request method.";
-        break;
-}
+include "/php/metodo.php";
 ?>
