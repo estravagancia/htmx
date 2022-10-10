0bb8009f97f6190e5330e39c5442aecba756c1f0
estravagancia <webmaster@estravagancia.com>
2022-10-10 12:43:10 +0200

    cambiado el title

--- mustache/contra-api.php
+++ mustache/contra-api.php
@@ -8,7 +8,7 @@ $_SESSION['token'] = md5(uniqid(mt_rand(), true));
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>X-CSRFToken</title>
+    <title>API HHTMX Mustache.js</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/htmx.org@1.8.0"></script>
